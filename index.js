3bb18c119842dc80076e5e3ecc2330f51c9bc313
FaisalArdhani <faislardhni@gmail.com>
2023-08-15 10:11:21 +0700

    update

--- index.js
+++ index.js
@@ -2,28 +2,35 @@
 
 const axios = require('axios')
 const { program } = require('commander')
+const chalk = require('chalk');
+const Table = require('cli-table3')
+
 
 program
     .version('1.0.0')
     .description('Github Profile CLI')
 
 program
-    .command('profile <username>') // --> this will enter a github username
-    .alias('p')
+    .command('profile <username>') // --> this will enter a github username.
+    .alias('p') // --> use 'p' as an alternative for profile.
     .description('Github Profile CLI')
     .action((username) => {
         axios
             .get(`https://api.github.com/users/${username}`)
             .then((response) => {
                 const user = response.data;
-                console.log(`
-            Username: ${user.login}
-            Name: ${user.name || 'N/A'}
-            Bio: ${user.bio || 'N/A'}
-            Followers: ${user.followers}
-            Following: ${user.following}
-            Public Repos: ${user.public_repos}
-        `)
+                const table = new Table()
+
+                table.push(
+                    [chalk.whiteBright.bold('Username'), chalk.blueBright(user.login)],
+                    [chalk.whiteBright.bold('Name'), chalk.blueBright(user.name || chalk.redBright('Has not provided a name'))],
+                    [chalk.whiteBright.bold('Bio'), chalk.blueBright(user.bio || chalk.redBright('No bio available'))],
+                    [chalk.whiteBright.bold('Followers'), chalk.blueBright(user.followers)],
+                    [chalk.whiteBright.bold('Following'), chalk.blueBright(user.following)],
+                    [chalk.whiteBright.bold('Public Repos'), chalk.blueBright(user.public_repos)],
+                    [chalk.whiteBright.bold('Location'), chalk.blueBright(user.location || chalk.redBright('Location not specified'))],
+                )
+                console.log(table.toString());
             })
             .catch((error) => {
                 console.error('Error fetching profile:', error.response.data.message)
